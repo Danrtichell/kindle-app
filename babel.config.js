7d95b5399e96552b244fb4c0abfaa35c7c19810a
Danritchell <dannyneverfade@yahoo.com>
2020-06-20 17:31:52 +0800

    added custom path aliases with typescript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          'test/*': ['./test/'],
+          '@modules': './src/modules',
+          '@enums': './src/enums',
+          '@constant': './src/constant',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
